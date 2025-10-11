45bf376f0b76a0ad71bd7702b00a14d965658820
Katarzyna Pilarczyk <kpilarczyk@edu.cdv.pl>
2025-10-11 17:08:44 +0200

    Lab2

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+KontoBankowe konto = new KontoBankowe();
+class KontoBankowe
+{
+    private double saldo;
+    public void Wplata(double kwota) { saldo += kwota; }
+    public void Wyplata(double kwota)
+    {
+        if (saldo > kwota)
+        {
+            saldo -= kwota;
+        }
+        else
+        {
+            Console.WriteLine("Nie masz wystarczająco pieniędzy na koncie");
+        }
+    }
+    public double PobierzSaldo() { return saldo; }
+}
+*/ 
+/*
+zadanie 6
 
-Console.WriteLine("Hello, World!");
+class Zwierze
+{
+    public void Jedz() => Console.WriteLine("Zwierzę je");
+}
+class Pies : Zwierze
+{
+    public void Szczekaj() => Console.WriteLine("Hau hau!");
+}
+
+class Kot: Zwierze
+{
+*/
